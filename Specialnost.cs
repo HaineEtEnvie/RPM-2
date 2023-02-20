f90173090cd7cf1a5d20ccefe054affd70898a5a
foxline3 <125448874+foxline3@users.noreply.github.com>
2023-02-20 16:20:47 +0300

    Add files via upload

---
+++ Specialnost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClassLibrary1
+{
+    public class Specialnost
+    {
+        private readonly string name61;
+        public string Name61 { get => name61; }
+        private readonly string name62;
+        public string Name62 { get => name62; }
+        public Specialnost(string name61, string name62)
+        {
+            this.name61 = name61;
+            this.name62 = name62;
+        }
+    }
+}
