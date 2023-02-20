15d393ad24fbabe92c8b40dd662445e8bd67aa9f
Neganov Mihail <haineenvie@gmail.com>
2023-02-20 18:52:24 +0300

    Global Izmeneniya

--- ClassLibrary1/Specialnost.cs
+++ ClassLibrary1/Specialnost.cs
@@ -3,18 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace ClassLibrary1
 {
     public class Specialnost
     {
-        private readonly string name;
-        public string Name6{ get => name; }
-        private readonly string reduction;
-        public string Reduction { get => reduction; }
-        public Specialnost(string name, string reduction)
-        {
-            this.name = name;
-            this.Reduction = reduction;
-        }
     }
 }
--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -2,95 +2,248 @@
 using System.Security.Cryptography.X509Certificates;
 using System.Xml;
 using System.Xml.Linq;
-
-while (true)
+class Program
 {
-    Console.WriteLine("Какой класс Вы хотите создать?");
-    switch (Console.ReadLine())
+    public static void Main()
     {
+        while (true)
+        {
+            Console.WriteLine("Какой класс Вы хотите создать?");
+            switch (Console.ReadLine())
+            {
 
-        case "Zaniatie": 
-            var zaniatie = CreateZaniatie(); // 1 вариант - занятие
-            Console.WriteLine($"\nПолученные данные:\n{zaniatie.Name2.Name21}"); // 2 вариант - аудитория
-            Console.WriteLine(zaniatie.Name2.Name22);// 2 вариант - аудитория
-            Console.WriteLine(zaniatie.Name2.Name23);// 2 вариант - аудитория
-            Console.WriteLine(zaniatie.Name3.Name1);// 3 вариант - дисциплина
-            Console.WriteLine(zaniatie.Name3.Shortname);// 3 вариант - дисциплина
-            Console.WriteLine(zaniatie.Name14.Name141);// 14 вариант - вид занятия
-            break;
-        case "Dyscyplina":
-            var dyscyplina = CreateDiscyplina();// 2 вариант - аудитория
-            Console.WriteLine(dyscyplina.Name1);
-            Console.WriteLine(dyscyplina.Shortname);
-            break;
-        case "VidZanyatiya":
-            var vidzanyatiya = CreateVidZanyatiya();// 3 вариант - дисциплина
-            Console.WriteLine(vidzanyatiya.Name141);
-            break;
-        case "Ayditoria":
-            var ayditoria = CreateAyditoria();// 14 вариант - вид занятия
-            Console.WriteLine(ayditoria.Name21);
-            Console.WriteLine(ayditoria.Name22);
-            Console.WriteLine(ayditoria.Name23);
-            break;
-        case "Specialnost":
-            var specialnost = CreateSpecialnost();// 3 вариант - дисциплина
-            Console.WriteLine(specialnost.Name61);
-            Console.WriteLine(specialnost.Name62);
-            break;
-        default:
-            Console.WriteLine("Такого класса не существует");
-            break;
+                case "Zaniatie":
+                    var zaniatie = CreateZaniatie(); // 1 вариант - занятие
+                    Console.WriteLine($"\nПолученные данные:\n{zaniatie.Ayditoria.Nazvanie}"); // 2 вариант - аудитория
+                    Console.WriteLine(zaniatie.Ayditoria.Posadmesta);// 2 вариант - аудитория
+                    Console.WriteLine(zaniatie.Ayditoria.Kolvookon);// 2 вариант - аудитория
+                    Console.WriteLine(zaniatie.Discyplina.Name1);// 3 вариант - дисциплина
+                    Console.WriteLine(zaniatie.Discyplina.Shortname);// 3 вариант - дисциплина
+                    Console.WriteLine(zaniatie.VidZanyatiya.Name141);// 14 вариант - вид занятия
+                    break;
+                case "Ayditoria":
+                    var ayditoria = CreateAyditoria();// 2 вариант - аудитория
+                    Console.WriteLine(ayditoria.Nazvanie);
+                    Console.WriteLine(ayditoria.Posadmesta);
+                    Console.WriteLine(ayditoria.Kolvookon);
+                    break;
+                case "Discyplina":
+                    var discyplina = CreateDiscyplina();// 3 вариант - дисциплина
+                    Console.WriteLine(discyplina.Name1);
+                    Console.WriteLine(discyplina.Shortname);
+                    break;
+                case "Gryppa":
+                    // 4 вариант
+                    break;
+                case "Student":
+                    // 5 вариант
+                    break;
+                case "Specialnost":
+                    // 6 вариант
+                    break;
+                case "Para":
+                    // 7 вариант
+                    break;
+                case "Smena":
+                    // 8 вариант
+                    break;
+                case "Sotrydnik":
+                    // 9 вариант
+                    break;
+                case "Doljnost":
+                    // 10 вариант
+                    break;
+                case "Podrazdelenie":
+                    // 11 вариант
+                    break;
+                case "Organizacia":
+                    // 12 вариант
+                    break;
+                case "Korpus":
+                    // 13 вариант
+                    break;
+                case "VidZanyatiya":
+                    var vidzanyatiya = CreateVidZanyatiya();// 14 вариант - дисциплина
+                    Console.WriteLine(vidzanyatiya.Name141);
+                    break;
+                case "Oborudovanie":
+                    // 15 вариант
+                    break;
+                case "KTP":
+                    // 16 вариант
+                    break;
+                case "Tema":
+                    // 17 вариант
+                    break;
+                case "Paragraf":
+                    // 18 вариант
+                    break;
+                case "Tema-VidZanyatiya":
+                    // 19 вариант
+                    break;
+                case "Material":
+                    // 20 вариант
+                    break;
+                case "Kompetenciya":
+                    // 21 вариант
+                    break;
+                case "ShapkaKTP":
+                    // 22 вариант
+                    break;
+                case "Vneurochka":
+                    // 23 вариант
+                    break;
+                default:
+                    Console.WriteLine("Такого класса не существует");
+                    break;
+
+            }
+        }
 
     }
-}
 
-static Zaniatie CreateZaniatie()
-{
-    var name2 = CreateAyditoria(); // 2 вариант - аудитория
-    var name3 = CreateDiscyplina(); // 3 вариант - дисциплина
-    var name14 = CreateVidZanyatiya(); // 14 вариант - вид занятия
-    var a = new Zaniatie(name2, name3, name14);
-    return a;
+    static Zaniatie CreateZaniatie()
+    {
+        return new Zaniatie(
+            CreateAyditoria(),
+            CreateDiscyplina(),
+            CreateVidZanyatiya());
+    }
 
-}
-static Ayditoria CreateAyditoria()
-{
-    Console.Write("Введите название аудитории: ");
-    var name21 = Console.ReadLine() ?? "Нет названия";
-    // ссылка на сотрудника
-    Console.Write("Введите кол-во посадочных мест: ");
-    var name22 = Console.ReadLine() ?? "Нет названия";
-    Console.Write("Введите кол-во окон: ");
-    var name23 = Console.ReadLine() ?? "Нет названия";
-    var a = new Ayditoria(name21, name22, name23);
-    return a;
-}
+    /// <summary>
+    /// 2 вариант - аудитория
+    /// </summary>
+    static Ayditoria CreateAyditoria()
+    {
+        Console.Write("Введите название аудитории: ");
+        var nazvanie = Console.ReadLine() ?? "Нет названия";
+        // ссылка на сотрудника
+        Console.Write("Введите кол-во посадочных мест: ");
+        var posadmesta = Console.ReadLine() ?? "Нет названия";
+        Console.Write("Введите кол-во окон: ");
+        var kolvookon = Console.ReadLine() ?? "Нет названия";
+        var a = new Ayditoria(nazvanie, posadmesta, kolvookon);
+        return a;
+    }
 
-static Discyplina CreateDiscyplina()
-{
-    Console.Write("Введите полное навазние дисциплины: ");
-    var name = (Console.ReadLine() ?? "Нет названия");
-    Console.Write("Введите сокращение дисциплины: ");
-    var shortname = (Console.ReadLine() ?? "Нет названия");
-    var a = new Discyplina(name, shortname);
-    return a;
-}
 
-static VidZanyatiya CreateVidZanyatiya()
-{
-    Console.Write("Введите вид занятия: ");
-    var name141 = new VidZanyatiya(Console.ReadLine() ?? "Нет названия");
-    return name141;
+    /// <summary>
+    /// 3 вариант - дисциплина
+    /// </summary>
+    /// <returns></returns>
+    static Discyplina CreateDiscyplina() // 3 вариант - дисциплина
+    {
+        Console.Write("Введите полное навазние дисциплины: ");
+        var name = (Console.ReadLine() ?? "Нет названия");
+        Console.Write("Введите сокращение дисциплины: ");
+        var shortname = (Console.ReadLine() ?? "Нет названия");
+        var a = new Discyplina(name, shortname);
+        return a;
+    }
+    static Gryppa CreateGryppa() // 4
+    {
+        throw new NotImplementedException();
+    }
 
-}
+    static Student CreateStudent() // 5
+    {
+        throw new NotImplementedException();
+    }
 
-static Specialnost CreateSpecialnost()
-{
-    Console.WriteLine("Введите название специальности: ");
-    var name = (Console.ReadLine() ?? "Нет названия");
-    Console.WriteLine("Введите сокращение специальности: ");
-    var reduction = (Console.ReadLine() ?? "Нет сокращения");
-    var a = new Specialnost(name61, name62);
-    return a;
+    static Specialnost CreateSpecialnost() // 6
+    {
+        throw new NotImplementedException();
+    }
+
+    static Para CreatePara() // 7
+    {
+        throw new NotImplementedException();
+    }
+
+    static Smena CreateSmena() // 8
+    {
+        throw new NotImplementedException();
+    }
+
+    static Sotrydnik CreateSotrydnik() // 9
+    {
+        throw new NotImplementedException();
+    }
+
+    static Doljnost CreateDoljnost() // 10
+    {
+        throw new NotImplementedException();
+    }
+
+    static Podrazdelenie CreatePodrazdelenie() // 11
+    {
+        throw new NotImplementedException();
+    }
+
+    static Organizacia CreateOrganizacia() // 12
+    {
+        throw new NotImplementedException();
+    }
+
+    static Korpus CreateKorpus() // 13
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// 14 вариант - вид занятия
+    /// </summary>
+    /// <returns></returns>
+    static VidZanyatiya CreateVidZanyatiya() // 14 вариант - вид занятия
+    {
+        Console.Write("Введите вид занятия: ");
+        var name141 = new VidZanyatiya(Console.ReadLine() ?? "Нет названия");
+        return name141;
+
+    }
+
+    static Gryppa CreateVidZanyatiyaGryppa() // 15
+    {
+        throw new NotImplementedException();
+    }
+
+    static KTP CreateKTP() // 16
+    {
+        throw new NotImplementedException();
+    }
+
+    static Tema CreateTema() // 17
+    {
+        throw new NotImplementedException();
+    }
+
+    static Paragraf CreateParagraf() // 18
+    {
+        throw new NotImplementedException();
+    }
+
+    static Tema_VidZanyatiya CreateTema_VidZanyatiya() // 19
+    {
+        throw new NotImplementedException();
+    }
+
+    static Material CreateMaterial() // 20
+    {
+        throw new NotImplementedException();
+    }
+
+    static Kompetenciya CreateKompetenciya() // 21
+    {
+        throw new NotImplementedException();
+    }
+
+    static ShapkaKTP CreateShapkaKTP() // 22
+    {
+        throw new NotImplementedException();
+    }
+
+    static Vneurochka CreateVneurochka() // 23
+    {
+        throw new NotImplementedException();
+    }
 }
