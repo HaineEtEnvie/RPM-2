008c4eb8bf23d3c802576282d656b35fa975deca
ArchieLich <118841308+ArchieLich@users.noreply.github.com>
2023-02-27 07:41:24 +0300

    Add files via upload

--- ClassLibrary1/Gryppa.cs
+++ ClassLibrary1/Gryppa.cs
@@ -8,5 +8,27 @@ namespace ClassLibrary1
 {
     public class Gryppa
     {
+        private readonly string name;
+        public string Name { get => name; }
+
+        private readonly string shortname;
+        public string Shortname { get => shortname; }
+        private readonly string chislo;
+        public string Chislo { get => chislo; }
+        private readonly string year;
+        public string Year { get => year; }
+       // private readonly Specialnost specialnost;
+        //public Specialnost Specialnost { get => specialnost; }
+        //private readonly Sotrydnik sotrydnik;
+        //public Sotrydnik Sotrydnik { get => sotrydnik; }
+        public Gryppa(string name, string shortname, string chislo, string year) // Specialnost specialnost, Sotrydnik sotrydnik// )
+        {
+            this.name = name;
+            this.shortname = shortname;
+            this.chislo = chislo;
+            this.year = year;
+            //this.specialnost = specialnost;
+           // this.sotrydnik=sotrydnik;
+        }
     }
 }
