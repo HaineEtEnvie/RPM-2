7d9e91fa06706687560a6b3601b80665dd8ac7f9
Squish21187 <vladmak58@gmail.com>
2023-02-27 14:19:56 +0300

    init

--- ClassLibrary1/Student.cs
+++ ClassLibrary1/Student.cs
@@ -8,5 +8,27 @@ namespace ClassLibrary1
 {
     public class Student
     {
+        private readonly string name;
+        public string Name { get => name; }
+
+        private readonly string familia;
+        public string Familia { get => familia; }
+        private readonly string otchestvo;
+        public string Otchestvo { get => otchestvo; }
+
+        private readonly Gryppa gryppa;
+        public Gryppa Gryppa { get => gryppa; }
+
+        public int Data { get => data; }
+        private readonly int data;
+
+        public Student(string name, string familia, string otchestvo, Gryppa gryppa, int data)
+        {
+            this.name = name;
+            this.familia = familia;
+            this.otchestvo = otchestvo;
+            this.gryppa = gryppa;
+            this.data = data;
+        }
     }
 }
