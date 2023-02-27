674fe894afee36e088f31e89a2ee54e06a653ea1
Squish21187 <vladmak58@gmail.com>
2023-02-27 15:57:07 +0300

    First commit

--- ClassLibrary1/Korpus.cs
+++ ClassLibrary1/Korpus.cs
@@ -8,5 +8,26 @@ namespace ClassLibrary1
 {
     public class Korpus
     {
+        private readonly string name;
+        public string Name { get => name; }
+        private readonly string adress;
+
+        public string Adress { get => adress; }
+        private readonly Sotrydnik sotrydnik;
+
+        private readonly Organizacia organization;
+        public Organizacia Organizacia { get => organization; }
+        private readonly DateTime dates;
+        public DateTime Date { get => dates; }
+
+        public Korpus(string name, string adress, Sotrydnik komendant, Organizacia org, DateTime date)
+        {
+            this.name = name;
+            this.adress = adress;
+            this.sotrydnik = komendant;
+            this.organization = org;
+            this.dates = date;
+
+        }
     }
 }
